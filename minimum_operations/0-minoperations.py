19a9f5f13071a3e9bdc19bd552f9c1de3679ce07
d3ng4 <denggi2000@gmail.com>
2023-01-23 12:34:07 -0800

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""Module calculate the min num of opration"""
+"""
+minimum operations
+"""
 
 
 def minOperations(n):
-    """ that calculates the fewest number of operations"""
-
-    temp = 1
-    counter = 0
-    cpyall = 1
-    if (n < 2):
-        return 0
-    if(n % 2 != 0):
-        cpyall = temp
-        temp = (2 * temp)
-        counter += 2
-        temp += cpyall
-        counter += 1
-    while(temp < n):
-        if ((2 * temp) <= n):
-            cpyall = temp
-            temp = (2 * temp)
-            counter += 2
-        elif((temp + cpyall) <= n):
-            temp = temp + cpyall
-            counter += 1
-
-    return counter
+    """
+    returns the fewest number of operation
+    """
+    a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+        b += 1
+    return a
